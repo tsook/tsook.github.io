b17c4bbea39248f498163cefc0aeed2a9ddbada3
tsook <kimts0830@gmail.com>
2023-04-16 17:05:18 +0900

    Updates

--- _site/assets/js/darkmode.js
+++ _site/assets/js/darkmode.js
@@ -1,31 +1,34 @@
 function toggleMode() {
     var element = document.body;
-    element.classList.toggle("body-dark");
+    element.classList.toggle("body-mode");
     var elements = document.getElementsByTagName("a");
     for (var i = 0; i < elements.length; i++) {
-        elements[i].classList.toggle("a-dark");
+        elements[i].classList.toggle("a-mode");
     }
     var elements = document.getElementsByTagName("b");
     for (var i = 0; i < elements.length; i++) {
-        elements[i].classList.toggle("b-dark");
+        elements[i].classList.toggle("b-mode");
     }
     var elements = document.getElementsByTagName("strong");
     for (var i = 0; i < elements.length; i++) {
-        elements[i].classList.toggle("strong-dark");
+        elements[i].classList.toggle("strong-mode");
     }
     var elements = document.getElementsByClassName("award");
     for (var i = 0; i < elements.length; i++) {
-        elements[i].classList.toggle("award-dark");
+        elements[i].classList.toggle("award-mode");
     }
     var elements = document.getElementsByClassName("icon");
     for (var i = 0; i < elements.length; i++) {
-        elements[i].classList.toggle("icon-dark");
+        elements[i].classList.toggle("icon-mode");
     }
     var element = document.getElementById('mode-toggle');
     element.classList.toggle("fa-moon");
     element.classList.toggle("fa-sun");
 }
 
+// if check prefered scheme
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    toggleMode();
+    var element = document.getElementById('mode-toggle');
+    element.classList.toggle("fa-moon");
+    element.classList.toggle("fa-sun");
 }
